03cd3d4382c634ac3e04b62523a406d4740d12d9
Tristan K <tristanmkernan@gmail.com>
2019-04-19 10:22:24 -0400

    fix: add docker cache busting to auto updater to ensure latest ytdl package installed on each build

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,10 @@ FROM tiangolo/uwsgi-nginx-flask:python3.7
 COPY requirements.txt /app/requirements.txt
 WORKDIR /app
 RUN pip install --upgrade pip
+
+# thanks to https://github.com/moby/moby/issues/22832
+ARG CACHE_DATE=2016-01-01
+
 RUN pip install -U -r /app/requirements.txt
 COPY . /app
 
--- ytdl_auto_upgrade.sh
+++ ytdl_auto_upgrade.sh
@@ -6,5 +6,6 @@
 # `RUN pip install -U -r /app/requirements.txt`
 # meaning that the package should be upgraded to the latest possible
 
-docker-compose build
+# cd /wherever/your/directory/is/located/
+docker-compose build --build-arg CACHE_DATE=$(date +%Y-%m-%d)
 docker-compose up -d worker  # only restart the worker containers
